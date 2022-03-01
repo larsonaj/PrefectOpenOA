7e3294805878965ad75a67c5d843510d6927b23e
larsonaj <andrew.j.larson12@gmail.com>
2022-02-28 17:16:49 -0700

    task shells

--- actions/run_dbx_notebook.py
+++ actions/run_dbx_notebook.py
@@ -1,5 +1,11 @@
 import prefect
+from prefect.tasks import databricks
 
-def run_dbx_notebook(nb_path, azure_conn):
-    
-
+def run_dbx_notebook(nb_path, azure_conn, cluster_id):
+    json = {'cluster_id': cluster_id,
+            'notebook_task': {
+                'notebook_path': nb_path 
+                }
+            }
+    run = databricks.DatabricksSubmitRun(json=json)
+    run(databricks_conn_secret=azure_conn)
