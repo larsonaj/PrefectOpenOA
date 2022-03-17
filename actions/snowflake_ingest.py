2206823f73a2ffec46904fcdfd58615546ba4453
larsonaj <andrew.j.larson12@gmail.com>
2022-03-17 17:37:37 -0600

    demo flow added

--- actions/snowflake_ingest.py
+++ actions/snowflake_ingest.py
@@ -3,7 +3,7 @@ from prefect.tasks import snowflake
 
 
 def ingest_scada_to_snowflake(account_prefix, wh_name, db_name, schema_name, user_name, username, password):
-    query_text = """"""
+    query_text = """select top 10 * from OpenOA_Scada"""
     snowflake.SnowflakeQuery(query=query_text, account=account_prefix, warehouse=wh_name, 
                             database=db_name, schema=schema_name, user=user_name)
 
--- flows/testint.py
+++ flows/testint.py
@@ -2,6 +2,7 @@ import prefect
 from prefect import Flow, task
 import sys
 from prefect.tasks import databricks
+from PrefectOpenOA.actions.snowflake_ingest import ingest_scada_to_snowflake
 
 sys.path.append('.')
 
@@ -16,13 +17,17 @@ import actions.dbx
 
 
 conn = {"host":"adb-7101253137415266.6.azuredatabricks.net",
-"token":"dapibf1a2e725dbe5a79cd1dd14a9a55dcbd"}
+        "token":"dapibf1a2e725dbe5a79cd1dd14a9a55dcbd"}
 
 json = {'existing_cluster_id': "0301-005003-1urzp405",
     'notebook_task': {
         'notebook_path': "/Users/alarson@captechventures.com/project_CapTech" 
         }
     }
+
+
+ingest_scada_to_snowflake
+
 
 with Flow("move-doc") as flow:
     notebook_run = databricks.DatabricksSubmitRun(json=json)
