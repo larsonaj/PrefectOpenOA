555fdd2696b99dafaf0850d15a6b050783dde470
larsonaj <andrew.j.larson12@gmail.com>
2022-05-10 15:10:23 -0600

    metadata driven flow

--- flows/Demo-Metadata.py
+++ flows/Demo-Metadata.py
@@ -3,11 +3,12 @@ import prefect
 # Import python helpers
 import json
 import datetime
+import sys
+
 
 # Import tasks
 from prefect.tasks import databricks
 from prefect import Flow, task
-import actions
 
 # Import helpers
 from prefect.client import Secret
@@ -20,28 +21,29 @@ from prefect.tasks import snowflake
 ## Parse metadata
 
 # storage
-git_raw = open('.\metadata\storage.json')
-git_parsed = json.loads(git_raw)
+with open(r'.\metadata\storage.json', 'r') as f:
+    git_parsed = json.loads(f.read())
 
 
 
 # task info
-snowflake_raw = open('.\metadata\adls_to_snowflake.json')
-snowflake_parsed = json.loads(snowflake_raw)
+with open(r'.\metadata\adls_to_snowflake.json', 'r') as f:
+    snowflake_parsed = json.loads(f.read())
+
+
+with open(r'.\metadata\databricks_notebook.json', 'r') as f:
+    dbx_parsed = json.loads(f.read())
+
+flow_storage = git_parsed['github_openoa']
 snowflake_conn = snowflake_parsed["snowflake_connection"]
-
-dbx_raw = open('.\metadata\databricks_notebook.json')
-dbx_parsed = json.loads(dbx_raw)
 dbx_run_info = dbx_parsed["databricks_openoa"]
-
-
 
 ## Configure Context
 storage = GitHub(
-    repo=git_parsed['repo'],
-    path=git_parsed['path'],
-    ref=git_parsed['branch'],
-    access_token_secret=git_parsed['secret_name']
+    repo=flow_storage['repo'],
+    path=flow_storage['path'],
+    ref=flow_storage['branch'],
+    access_token_secret=flow_storage['secret_name']
 )
 
 run_config = UniversalRun(labels=['DESKTOP-ETPQA0T'])
@@ -61,13 +63,16 @@ sn_password = PrefectSecret(sn_pw_name)
 
 ## Task specifications
 
-snowflake_specs = snowflake_parsed['task_specs']
+# snowflake_specs = snowflake_parsed['task_specs']
+snowflake_task_specs = {'max_retries':5,
+                        'retry_delay':datetime.timedelta(seconds=5)}
 
 query = snowflake_parsed['queries']
 
 query_text = query['demo_query']
 
-## Build tasks
+
+# Build tasks
 @task(name='Printer Task')
 def say_hello(printer):
     logger = prefect.context.get("logger")
@@ -78,12 +83,14 @@ def new_printer(item):
     print(item)
     pass
 
+
+
 ## Build flow
 
 with Flow("run-snowflake-demo-meta", run_config=run_config, storage=storage) as flow:
     password = PrefectSecret('SNOWFLAKE_PW')
     snowflake_run = snowflake.SnowflakeQuery(query=query_text, account=account_prefix, warehouse=wh_name,
-                        database=db_name, schema=schema_name, user=user_name, **snowflake_specs)
+                        database=db_name, schema=schema_name, user=user_name, **snowflake_task_specs)
     results = snowflake_run(password=password)
     logging = say_hello(results)
     print_results = new_printer('test')
